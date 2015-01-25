a579a17937de2f0ab230298b360f55d1047d7ed3
Champ Davis <champwdavis@gmail.com>
2015-01-25 12:44:39 -0500

    tweet list

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,20 @@
 class TweetsController < ApplicationController
 
+
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
+		@tweets = current_user.tweets
+
 	end
 
 	def create 
-		@tweet = Tweet.create(tweet_params)
+		@tweet = Tweet.new(tweet_params)
+		@tweet.user=current_user
+		@tweet.save
+		@tweets = current_user.tweets
+		
 		flash.now[:success] = "Saved Your Tweet"
 		render 'new'
 	end
